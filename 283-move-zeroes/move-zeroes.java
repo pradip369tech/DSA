97a33d08a5916b13ff3da14c4ce7ac21dd6ae21c
Pradipta pal <pradipta.pal369.tech@gmail.com>
2026-09-02 00:56:37 +0530

    Time: 2 ms (91.89%) | Memory: 47.9 MB (20.27%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,39 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length == 1){
-            System.out.print('[');
-            System.out.print(nums[0] + "]");
-            
-        
-        }
-        else{
+        int insertPos = 0;
 
-            int key = 0;
-            int i = 0;
-            while(i < nums.length && key < nums.length){
-                if(nums[key] == 0){
-                    i = key +1;
-                    while(i != nums.length && nums[i] == 0){
-                        i++;
-                    }
-                    if(i != nums.length){
-                        int temp = nums[i];
-                        nums[i] = nums[key];
-                        nums[key] =temp ;
-                    }
-                    key++;
-                }
-                else{
-                    key++;
-                }
-            }
         
-        
-            System.out.print('[');
-            for( i = 0 ; i< nums.length; i++){
-                if(i != nums.length -1) System.out.print(nums[i] + ",");
-                else System.out.print(nums[i] + "]");
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
             }
+        }
+
+        
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
         }
     }
 }
